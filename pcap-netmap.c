e896f2726360165fecdd48aa75c496cbf3129411
Luigi Rizzo <rizzo@iet.unipi.it>
2014-10-06 09:08:14 -0700

    sync with the code in FreeBSD: - fix error when no bpf_filter is passed; - fix flagshigh usage - typo in copyright and whitespace

--- pcap-netmap.c
+++ pcap-netmap.c
@@ -11,7 +11,7 @@
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``S IS''AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS''AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
@@ -43,7 +43,9 @@
 #include "pcap-int.h"
 
 /*
- * This code is meant to build also on older versions of libpcap.
+ * $FreeBSD: head/lib/libpcap/pcap-netmap.c 272659 2014-10-06 15:48:28Z luigi $
+ *
+ * This code is meant to build also on other versions of libpcap.
  *
  * older libpcap miss p->priv, use p->md.device instead (and allocate).
  * Also opt.timeout was in md.timeout before.
@@ -64,7 +66,6 @@
  * remap to IFF_PROMISC on linux
  */
 #define IFF_PPROMISC	IFF_PROMISC
-#define ifr_flagshigh	ifr_flags
 #endif /* linux */
 
 struct pcap_netmap {
@@ -74,6 +75,7 @@ struct pcap_netmap {
 	int must_clear_promisc;	/* flag */
 	uint64_t rx_pkts;	/* # of pkts received before the filter */
 };
+
 
 static int
 pcap_netmap_stats(pcap_t *p, struct pcap_stat *ps)
@@ -86,16 +88,19 @@ pcap_netmap_stats(pcap_t *p, struct pcap_stat *ps)
 	return 0;
 }
 
+
 static void
 pcap_netmap_filter(u_char *arg, struct pcap_pkthdr *h, const u_char *buf)
 {
 	pcap_t *p = (pcap_t *)arg;
 	struct pcap_netmap *pn = NM_PRIV(p);
+	const struct bpf_insn *pc = p->fcode.bf_insns;
 
 	++pn->rx_pkts;
-	if (bpf_filter(p->fcode.bf_insns, buf, h->len, h->caplen))
+	if (pc == NULL || bpf_filter(pc, buf, h->len, h->caplen))
 		pn->cb(pn->cb_arg, h, buf);
 }
+
 
 static int
 pcap_netmap_dispatch(pcap_t *p, int cnt, pcap_handler cb, u_char *user)
@@ -114,13 +119,16 @@ pcap_netmap_dispatch(pcap_t *p, int cnt, pcap_handler cb, u_char *user)
 			return PCAP_ERROR_BREAK;
 		}
 		/* nm_dispatch won't run forever */
+
 		ret = nm_dispatch((void *)d, cnt, (void *)pcap_netmap_filter, (void *)p);
 		if (ret != 0)
 			break;
-		poll(&pfd, 1, p->the_timeout);
+		errno = 0;
+		ret = poll(&pfd, 1, p->the_timeout);
 	}
 	return ret;
 }
+
 
 /* XXX need to check the NIOCTXSYNC/poll */
 static int
@@ -130,6 +138,7 @@ pcap_netmap_inject(pcap_t *p, const void *buf, size_t size)
 
 	return nm_inject(d, buf, size);
 }
+
 
 static int
 pcap_netmap_ioctl(pcap_t *p, u_long what, uint32_t *if_flags)
@@ -151,20 +160,27 @@ pcap_netmap_ioctl(pcap_t *p, u_long what, uint32_t *if_flags)
 	switch (what) {
 	case SIOCSIFFLAGS:
 		ifr.ifr_flags = *if_flags;
+#ifdef __FreeBSD__
 		ifr.ifr_flagshigh = *if_flags >> 16;
+#endif /* __FreeBSD__ */
 		break;
 	}
 	error = ioctl(fd, what, &ifr);
-	fprintf(stderr, "%s %s ioctl 0x%lx returns %d\n", __FUNCTION__,
-		d->req.nr_name, what, error);
-	if (error)
-		return -1;
-	switch (what) {
-	case SIOCGIFFLAGS:
-		*if_flags = ifr.ifr_flags | (ifr.ifr_flagshigh << 16);
+	if (!error) {
+		switch (what) {
+		case SIOCGIFFLAGS:
+			*if_flags = ifr.ifr_flags;
+#ifdef __FreeBSD__
+			*if_flags |= (ifr.ifr_flagshigh << 16);
+#endif /* __FreeBSD__ */
+		}
 	}
-	return 0;
+#ifdef linux
+	close(fd);
+#endif /* linux */
+	return error ? -1 : 0;
 }
+
 
 static void
 pcap_netmap_close(pcap_t *p)
@@ -188,6 +204,7 @@ pcap_netmap_close(pcap_t *p)
 	pcap_cleanup_live_common(p);
 }
 
+
 static int
 pcap_netmap_activate(pcap_t *p)
 {
@@ -206,7 +223,8 @@ pcap_netmap_activate(pcap_t *p)
 		pcap_cleanup_live_common(p);
 		return (PCAP_ERROR);
 	}
-	fprintf(stderr, "%s device %s priv %p fd %d ports %d..%d\n",
+	if (0)
+	    fprintf(stderr, "%s device %s priv %p fd %d ports %d..%d\n",
 		__FUNCTION__, p->opt.source, d, d->fd,
 		d->first_rx_ring, d->last_rx_ring);
 	pn->d = d;
@@ -230,8 +248,10 @@ pcap_netmap_activate(pcap_t *p)
 	p->setnonblock_op = pcap_setnonblock_fd;
 	p->stats_op = pcap_netmap_stats;
 	p->cleanup_op = pcap_netmap_close;
+
 	return (0);
 }
+
 
 pcap_t *
 pcap_netmap_create(const char *device, char *ebuf, int *is_ours)
